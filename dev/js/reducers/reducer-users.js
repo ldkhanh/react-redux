8ee62c4ccb4318dd30088741ab79c46004208f90
khanhduyle <khanhduyle@assembla.com>
2017-09-02 20:28:32 -0700

    first commit

---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,28 @@
+export default function() {
+	return [
+		{
+			id: 1,
+			first: "Bucky",
+			last: "Roberts",
+			age: 71,
+			description: "Bucky is a React developer and Youtuber",
+			thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+		},
+		{
+			id: 2,
+			first: "Khanh",
+			last: "Le",
+			age: 27,
+			description: "Khanh is a React developer and Youtuber",
+			thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+		},
+		{
+			id: 3,
+			first: "Tom",
+			last: "Roberts",
+			age: 45,
+			description: "Bucky is a React developer and Youtuber",
+			thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+		},
+	]
+}
